6cd27e46206df8b41656d1e3f86bcedcc16ca8f6
Mark Schifflin <randy.schifflin@gmail.com>
2021-02-23 22:39:23 -0800

    Block on drop to clean up all outstanding connections

--- gudp/src/connection/listener.rs
+++ gudp/src/connection/listener.rs
@@ -16,9 +16,21 @@ pub struct Listener {
 
 impl Drop for Listener {
   fn drop(&mut self) {
-    (self.on_close)().unwrap_or_else(|e| {
-      warn!("Could not close listening socket on drop: {}. Resource may leak!", e)
-    });
+    (self.on_close)().map(|_| {
+      // NOTE: This is a blocking call, waiting for the daemon thread to close its sender.
+      // The reason being, a try_iter loop would be racy-
+      //   it's possible after we iterate but before we drop, a new item is sent.
+      //   that item would be lost, and the io on the daemon side would never be cleaned up.
+      // But it can be very counter-intuitive for a Destructor to block!!
+      // We might want a recv_timeout here just in case something strange occurs.
+      for awaiting_conn in self.rx.iter() {
+        if let FromDaemon::Connection(on_write, shared, id) = awaiting_conn {
+          // Build and drop any awaiting connections until the daemon closes its sender
+          // Dropping a connection guarantees it will eventually be cleaned up
+          Connection::new(on_write, shared, id);
+        }
+      }
+    }).unwrap_or_else(|e| { warn!("Could not close listening socket on drop: {}. Resource may leak!", e); });
   }
 }
 
--- gudp/src/daemon/listen_close_event.rs
+++ gudp/src/daemon/listen_close_event.rs
@@ -16,13 +16,14 @@ pub fn handle(mut token_entry: TokenEntry, poll: &Poll) {
 
     // All listeners are Passive sockets
     PeerType::Passive { ref mut listen, ref peers, .. } => {
-      *listen = None;
+      *listen = None; // Closes the channel the Listener awaits new connections on, unblocking the listener's Drop impl.
 
       // We can free the resource if there are no peers and we aren't listening
       if peers.len() == 0 {
         poll::deregister_io(poll, &mut socket.io);
         token_entry.remove();
       }
+      // The most common case is that peers exist but time out eventually, and the resource will be cleaned up then.
     }
   }
 }
